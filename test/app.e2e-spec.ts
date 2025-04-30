7931a0a26b8fc197e3acee59c4bf5bc53960f7ba
Lux <152927191+Luxw098@users.noreply.github.com>
2025-04-30 15:13:33 +0100

    add changeme's

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/api (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api')
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => res.status == "ok");
   });
 });
